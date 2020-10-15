75f68636d35f39d60f0eafb457bd39237cbe80e3
Isaiah Mazon <37008425+isamazon@users.noreply.github.com>
2020-10-15 14:19:15 -0700

    added some JS

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,19 @@
 const app = () => {
-    const song = document.querySelector('.song');
-}
+    const song = document.querySelector(".song");
+    const play = document.querySelector(".play");
+    const outline = document.querySelector(".moving-outline circle");
+    const video = document.querySelector(".vid-container video");
+    //Sounds
+    const sounds = document.querySelectorAll(".sound-picker button");
+    //Time Display
+    const timeDisplay = document.querySelector(".time-display");
+    const outlineLength = outline.getTotalLength();
+    console.log(outlineLength);
+    //duration
+    let fakeDuration = 600;
+
+    outline.style.strokeDasharray = outlineLength
+    outline.style.strokeDashoffset = outlineLength;
+    
+};   
+
