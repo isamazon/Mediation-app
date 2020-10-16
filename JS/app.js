d0f93c9d0adc0e8cf4c20db30e372a36c14573a1
Isaiah Mazon <37008425+isamazon@users.noreply.github.com>
2020-10-15 17:34:29 -0700

    created a function to stop and play background noise
    created a function to stop and play background noise and messed around with some css

--- JS/app.js
+++ JS/app.js
@@ -1,19 +1,36 @@
 const app = () => {
-    const song = document.querySelector(".song");
-    const play = document.querySelector(".play");
-    const outline = document.querySelector(".moving-outline circle");
-    const video = document.querySelector(".vid-container video");
-    //Sounds
-    const sounds = document.querySelectorAll(".sound-picker button");
-    //Time Display
-    const timeDisplay = document.querySelector(".time-display");
+    const song = document.querySelector('.song');
+    const play = document.querySelector('.play');
+    const outline = document.querySelector('.moving-outline circle');
+    const video = document.querySelector('.video-container video');
+
+    // sounds
+    const sounds = document.querySelectorAll('.sound-picker button');
+    // Time displat
+    const timeDisplay = document.querySelector('.time-display');
+    // get the length of the outline
     const outlineLength = outline.getTotalLength();
-    console.log(outlineLength);
-    //duration
+    // Duration
     let fakeDuration = 600;
 
-    outline.style.strokeDasharray = outlineLength
+    outline.style.strokeDasharray = outlineLength;
     outline.style.strokeDashoffset = outlineLength;
-    
-};   
 
+    //play sound
+    play.addEventListener('click', () => {
+        checkPlaying(song);
+    });
+
+    // Create a function specific to stop and play the song/sounds
+    const checkPlaying = song => {
+        if (song.paused) {
+            song.play();
+            video.play();
+            play.src = '../svg/pause.svg';
+        } else {
+            song.pause();
+            video.pause();
+            play.src = '../svg/play.svg';
+        }
+    }
+}
